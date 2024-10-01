1f528a965b1e5ecab6368aedbbf1445efdaee215
Lucas <berg@student.ubc.ca>
2024-10-01 16:21:45 -0700

    Added tests for Planet class Added a few more getters and setters to Planet class Modified the updateVelocity() method in Planet to give the correct velocity

--- src/main/model/Planet.java
+++ src/main/model/Planet.java
@@ -3,16 +3,15 @@ import java.util.ArrayList;
 
 public class Planet {
     float mass;
-    float xpos;
-    float ypos;
-    float dxpos;
-    float dypos;
+    double xpos;
+    double ypos;
+    double dxpos;
+    double dypos;
     int width = 500;
     int height = 500;
 
     // REQUIRES: mass > 0 && -width ≤ xpos ≤ width && -height ≤ ypos ≤ height
     // EFFECTS: creates a planet with given mass, xpos, and ypos
-
     public Planet(float mass, float xpos, float ypos) {
         this.xpos = xpos;
         this.ypos = ypos;
@@ -20,40 +19,68 @@ public class Planet {
         this.dypos = 0;
         this.mass = mass;
     }
+
     // MODIFIES: this
     // EFFECTS: moves a planet's xpos and ypos by dxpos and dypos
-
     public void updatePos() {
         this.xpos += dxpos;
         this.ypos += dypos;
     }
-    public float getXPos() {
+
+    public double getXPos() {
         return xpos;
     }
-    public float getYPos() {
+
+    public void setXPos(float xpos) {
+        this.xpos = xpos;
+    }
+
+    public double getYPos() {
         return ypos;
     }
-    public float getDXPos() {
+
+    public void setYPos(float ypos) {
+        this.ypos = ypos;
+    }
+
+    public double getDXPos() {
         return dxpos;
     }
-    public float getDYPos() {
+
+    public void setDXPos(float dxpos) {
+        this.dxpos = dxpos;
+    }
+
+    public double getDYPos() {
         return dypos;
     }
-    public float getMass() {
+
+    public void setDYPos(float dypos) {
+        this.dypos = dypos;
+    }
+
+    public double getMass() {
         return mass;
     }
-    // MODIFIES: this
-    // REQUIRES: G > 0
-    // EFFECTS: updates dxpos and dypos based on the positions of other planets
 
-    public void updateVelocities(ArrayList<Planet> planets, float G) {
-        float currentXVelocity = 0;
-        float currentYVelocity = 0;
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+
+    // MODIFIES: this
+    // REQUIRES: G > 0, ArrayList<Planet> planets does not contain planet
+    // EFFECTS: updates dxpos and dypos based on the positions of other planets
+    public void updateVelocity(ArrayList<Planet> planets, float G) {
+        double currentXVelocity = 0;
+        double currentYVelocity = 0;
         for (Planet planet : planets) {
-            float dx = this.xpos - planet.getXPos();
-            float dy = this.ypos - planet.getYPos();
-            currentXVelocity += (this.mass*planet.getMass()*G)/(dx*dx);
-            currentYVelocity += (this.mass*planet.getMass()*G)/(dy*dy);
+            double dx = planet.getXPos() - this.xpos;
+            double dy = planet.getYPos() - this.ypos;
+            double distance = Math.sqrt((dx*dx) + (dy*dy));
+            double theta = Math.atan(dy/dx);
+            double gravity = (this.mass*planet.getMass()*G)/(distance*distance);
+            currentXVelocity += (gravity*dy/distance);
+            currentYVelocity += (gravity*dx/distance);
         }
         this.dxpos = currentXVelocity;
         this.dypos = currentYVelocity;
--- src/test/model/TestPlanet.java
+++ src/test/model/TestPlanet.java
@@ -1,19 +1,97 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestPlanet {
-    
+    Planet testPlanet;
+    Planet otherPlanet;
+    Planet anotherPlanet;
+    ArrayList<Planet> testPlanets;
+    float G;
     @BeforeEach
     void runBefore() {
-
+        testPlanet = new Planet(10, 0, 0);
+        otherPlanet = new Planet(100, 10, 0);
+        anotherPlanet = new Planet(100, 0, 10);
+        G = 1;
+        testPlanets = new ArrayList<Planet>();
+        testPlanets.add(otherPlanet);
+        testPlanets.add(anotherPlanet);
     }
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testConstructor() {
+        assertEquals((float) 10, testPlanet.getMass());
+        assertEquals((float) 0, testPlanet.getXPos());
+        assertEquals((float) 0, testPlanet.getYPos());
+        assertEquals((float) 0, testPlanet.getDXPos());
+        assertEquals((float) 0, testPlanet.getDYPos());
+    }
+
+    @Test
+    void testUpdatePos() {
+        testPlanet.setDXPos(10);
+        testPlanet.setDYPos(10);
+        assertEquals((float) 0, testPlanet.getXPos());
+        assertEquals((float) 0, testPlanet.getYPos());
+        assertEquals((float) 10, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+        testPlanet.updatePos();
+        assertEquals((float) 10, testPlanet.getXPos());
+        assertEquals((float) 10, testPlanet.getYPos());
+        assertEquals((float) 10, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+    }
+
+    @Test
+    void testUpdatePosMultipleTimes() {
+        testPlanet.setDXPos(20);
+        testPlanet.setDYPos(10);
+        assertEquals((float) 0, testPlanet.getXPos());
+        assertEquals((float) 0, testPlanet.getYPos());
+        assertEquals((float) 20, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+        testPlanet.updatePos();
+        assertEquals((float) 20, testPlanet.getXPos());
+        assertEquals((float) 10, testPlanet.getYPos());
+        assertEquals((float) 20, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+        testPlanet.updatePos();
+        assertEquals((float) 40, testPlanet.getXPos());
+        assertEquals((float) 20, testPlanet.getYPos());
+        assertEquals((float) 20, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+    }
+    
+    @Test 
+    void testUpdateVelocity() {
+        testPlanet.updateVelocity(testPlanets, G);
+        assertEquals((float) 0, testPlanet.getXPos());
+        assertEquals((float) 0, testPlanet.getYPos());
+        assertEquals((float) 10, testPlanet.getDXPos());
+        assertEquals((float) 10, testPlanet.getDYPos());
+    }
+
+    @Test 
+    void testUpdateVelocityMultipleTimes() {
+        testPlanet.updateVelocity(testPlanets, G);
+        otherPlanet.setMass(1000);
+        otherPlanet.setXPos(100);
+        otherPlanet.setYPos(100);
+        double distance = Math.sqrt((100*100) + (100*100));
+        double gravity = (1000)/(distance*distance);
+        double xvelocity = 10+gravity*1000/distance;
+        double yvelocity = gravity*1000/distance ;
+        testPlanet.updateVelocity(testPlanets, G);
+        assertEquals((float) 0, testPlanet.getXPos());
+        assertEquals((float) 0, testPlanet.getYPos());
+        assertEquals(xvelocity, testPlanet.getDXPos());
+        assertEquals(yvelocity, testPlanet.getDYPos());
     }
 }
