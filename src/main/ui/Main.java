60e6bd06c58c9d5a215271813c248159532a26fe
Lucas <berg@student.ubc.ca>
2024-10-07 14:06:28 -0700

    Removed unused imports and changed initial G

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,12 +1,10 @@
 package ui;
 
 import model.NBodySimulation;
-import model.Planet;
-import ui.TextViewer;
 
 public class Main {
     public static void main(String[] args) {
-        int initialG = 1;
+        float initialG = (float) 0.1;
         NBodySimulation simulation = new NBodySimulation(initialG);
         TextViewer viewer = new TextViewer(simulation);
         viewer.start(); 
