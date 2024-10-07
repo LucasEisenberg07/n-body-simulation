12c440f333f9979e7f3b5cce69a6b6ac2dbd4acb
Lucas <berg@student.ubc.ca>
2024-10-06 18:30:10 -0700

    Removed unused import in TestNBodySimulation

--- src/test/model/TestNBodySimulation.java
+++ src/test/model/TestNBodySimulation.java
@@ -2,7 +2,6 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.util.ArrayList;
 
