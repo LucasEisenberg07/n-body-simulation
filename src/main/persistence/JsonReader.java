b00428eb345c2dc41af5d8d5bde0c83cf80465db
Lucas <berg@student.ubc.ca>
2024-10-30 15:13:36 -0700

    Added data to .gitignore

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -31,11 +31,9 @@ public class JsonReader {
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
         StringBuilder contentBuilder = new StringBuilder();
-
         try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
             stream.forEach(s -> contentBuilder.append(s));
         }
-
         return contentBuilder.toString();
     }
 
